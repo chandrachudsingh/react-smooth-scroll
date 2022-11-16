bbb7468d7410f30a935aaf106dfb14ac6682bf47
ccsc <chandrachudsingh81@gmail.com>
2022-11-16 12:40:13 +0530

    Signin page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages";
+import SigninPage from "./pages/signin";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" component={Home} element={<Home />} exact />
+        <Route
+          path="/signin"
+          component={SigninPage}
+          element={<SigninPage />}
+          exact
+        />
+      </Routes>
     </Router>
   );
 }
---
+++ src/pages/signin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SigninPage = () => {
+  return (
+    <div>
+      <h2>Signin Page</h2>
+    </div>
+  );
+};
+
+export default SigninPage;
